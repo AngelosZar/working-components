0a4855e8123d35b632bed58b4b7163d88083e0d7
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2024-02-23 19:00:44 +0100

    q

--- Experiment/actualPage.mjs
+++ Experiment/actualPage.mjs
@@ -7,11 +7,58 @@ import { rainyProdEndPoints } from "./modules.mjs";
 // Imported functions
 import { doFetchData } from "./modules.mjs";
 //      ------- Working on -------
+// creating cart component
 const createCart = function () {
-  console.log("yabadabadu");
+  const cart = localStorage.getItem("cart");
+  console.log("yabadabadu1");
+  if (!cart) {
+    localStorage.setItem("cart", JSON.stringify([]));
+  }
+  console.log("yabadabadu2");
+};
+const addToCart = function (product) {
+  console.log("gotcha");
 };
 //      ------- Working on -------
 //  generate html from the rainydays array
+{
+  /* <div class="content-container">
+            <div class="two-column">
+                <div class="column-left">
+                    <a href="../html.files/a-jacket-spesific.html">
+                        <div class="card">
+                            <div class="card-image"><img src="../imgs/Product-imgs/the-explorer.png"
+                                    alt="the motorist jacket">
+                            </div>
+                            <div class="card-text-field">
+                                <p class="product-ttl"> The explorer</p>
+                                <p class="product-txt"> With fully sealed seams, it’s durably waterproof,
+                                    completel windproof, and highly breathable.
+                                </p>
+                                <p class="product-price">2500 Kr</p>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="column-right">
+                    <a href="html.files/a-jacket-spesific.html">
+                        <div class="card">
+                            <div class="card-image"><img src="../imgs/Product-imgs/hd-hiker.png " alt="Xd Hiker jacket">
+                            </div>
+                            <div class="card-text-field">
+                                <p class="product-ttl">Xd Hiker</p>
+                                <p class="product-txt">This bound to be your new go-to hiking shell. Fully
+                                    waterproof,
+                                    breathable, and designed withstretch.</p>
+                                <p class="product-price">2500 Kr</p>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div> */
+}
 function genProdHtml(raincoat) {
   // console.log(raincoat);
   const productContainer = document.createElement("div");
@@ -32,13 +79,9 @@ async function displayRainCoatsLi(rainCoats) {
   });
 }
 async function main() {
-  try {
-    const { data: rainCoats } = await doFetchData(rainyProdEndPoints);
-    displayRainCoatsLi(rainCoats);
-    console.log(raincoats);
-  } catch (error) {
-    console.error();
-  }
+  createCart();
+  const { data: rainCoats } = await doFetchData(rainyProdEndPoints);
+  displayRainCoatsLi(rainCoats);
 }
 
 main();
